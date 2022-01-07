82582ec7d2aebbe08e5f9a6b0d2f734f1367f735
Orel <Orely4242@gmail.com>
2022-01-07 10:01:04 +0200

    beyond0lte: adapt for Arrow 12

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond0lte.mk
+    $(LOCAL_DIR)/arrow_beyond0lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond0lte-eng \
-    lineage_beyond0lte-user \
-    lineage_beyond0lte-userdebug
+    arrow_beyond0lte-eng \
+    arrow_beyond0lte-user \
+    arrow_beyond0lte-userdebug
